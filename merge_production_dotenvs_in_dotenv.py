84c078bcbfc61f585c885604309aef5d337da072
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-13 08:43:40 +0500

    ci: add coverage badge

--- merge_production_dotenvs_in_dotenv.py
+++ merge_production_dotenvs_in_dotenv.py
@@ -9,6 +9,7 @@ PRODUCTION_DOTENV_FILES = [
     PRODUCTION_DOTENVS_DIR / ".django",
     PRODUCTION_DOTENVS_DIR / ".postgres",
     PRODUCTION_DOTENVS_DIR / ".dockerHub",
+    PRODUCTION_DOTENVS_DIR / ".githubToken",
 ]
 DOTENV_FILE = BASE_DIR / ".env"
 
